963b21416e78a3b0274c250cb8310c8e06d3a2db
LieuTran <tranlieu329@gmail.com>
2020-10-23 22:20:17 +0700

    form Phong Ban

--- DuAn_LMB_NhanSu/US_Control/CT_PhongBan.Designer.cs
+++ DuAn_LMB_NhanSu/US_Control/CT_PhongBan.Designer.cs
@@ -31,20 +31,20 @@
             this.label1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
             this.Luoi_PhongBan = new System.Windows.Forms.DataGridView();
-            this.MaPhongBan = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.TenPhongBan = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.SoNhanVien = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.bt_Them = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnCapNhat = new System.Windows.Forms.Button();
+            this.btnThem = new System.Windows.Forms.Button();
             this.txtSoLNhanVien = new System.Windows.Forms.TextBox();
             this.txtTenPhongBan = new System.Windows.Forms.TextBox();
             this.txtMaPhongBan = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.lbmachinhanh = new System.Windows.Forms.Label();
+            this.lbMaChiNhanh = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.MaPhongBan = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TenPhongBan = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SoNhanVien = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.Luoi_PhongBan)).BeginInit();
             this.SuspendLayout();
@@ -53,31 +53,33 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(315, -4);
+            this.label1.Location = new System.Drawing.Point(420, -5);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(157, 33);
+            this.label1.Size = new System.Drawing.Size(204, 42);
             this.label1.TabIndex = 0;
             this.label1.Text = "Phòng Ban";
             // 
             // panel1
             // 
             this.panel1.Controls.Add(this.Luoi_PhongBan);
-            this.panel1.Controls.Add(this.button3);
-            this.panel1.Controls.Add(this.button2);
-            this.panel1.Controls.Add(this.bt_Them);
+            this.panel1.Controls.Add(this.btnXoa);
+            this.panel1.Controls.Add(this.btnCapNhat);
+            this.panel1.Controls.Add(this.btnThem);
             this.panel1.Controls.Add(this.txtSoLNhanVien);
             this.panel1.Controls.Add(this.txtTenPhongBan);
             this.panel1.Controls.Add(this.txtMaPhongBan);
             this.panel1.Controls.Add(this.label6);
             this.panel1.Controls.Add(this.label5);
             this.panel1.Controls.Add(this.label4);
-            this.panel1.Controls.Add(this.lbmachinhanh);
+            this.panel1.Controls.Add(this.lbMaChiNhanh);
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.panel1.Location = new System.Drawing.Point(14, 15);
+            this.panel1.Location = new System.Drawing.Point(19, 18);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(772, 449);
+            this.panel1.Size = new System.Drawing.Size(1029, 553);
             this.panel1.TabIndex = 1;
             // 
             // Luoi_PhongBan
@@ -89,82 +91,78 @@
             this.TenPhongBan,
             this.SoNhanVien});
             this.Luoi_PhongBan.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.Luoi_PhongBan.Location = new System.Drawing.Point(0, 285);
+            this.Luoi_PhongBan.Location = new System.Drawing.Point(0, 351);
+            this.Luoi_PhongBan.Margin = new System.Windows.Forms.Padding(4);
             this.Luoi_PhongBan.Name = "Luoi_PhongBan";
-            this.Luoi_PhongBan.Size = new System.Drawing.Size(772, 164);
+            this.Luoi_PhongBan.RowHeadersWidth = 51;
+            this.Luoi_PhongBan.Size = new System.Drawing.Size(1029, 202);
             this.Luoi_PhongBan.TabIndex = 12;
+            this.Luoi_PhongBan.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.Luoi_PhongBan_CellClick);
             // 
-            // MaPhongBan
+            // btnXoa
             // 
-            this.MaPhongBan.HeaderText = "Mã Phòng Ban";
-            this.MaPhongBan.Name = "MaPhongBan";
+            this.btnXoa.Location = new System.Drawing.Point(893, 288);
+            this.btnXoa.Margin = new System.Windows.Forms.Padding(4);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(100, 28);
+            this.btnXoa.TabIndex = 11;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
             // 
-            // TenPhongBan
+            // btnCapNhat
             // 
-            this.TenPhongBan.HeaderText = "Tên Phòng Ban";
-            this.TenPhongBan.Name = "TenPhongBan";
+            this.btnCapNhat.Location = new System.Drawing.Point(468, 288);
+            this.btnCapNhat.Margin = new System.Windows.Forms.Padding(4);
+            this.btnCapNhat.Name = "btnCapNhat";
+            this.btnCapNhat.Size = new System.Drawing.Size(100, 28);
+            this.btnCapNhat.TabIndex = 10;
+            this.btnCapNhat.Text = "Cập Nhật";
+            this.btnCapNhat.UseVisualStyleBackColor = true;
+            this.btnCapNhat.Click += new System.EventHandler(this.btnCapNhat_Click);
             // 
-            // SoNhanVien
+            // btnThem
             // 
-            this.SoNhanVien.HeaderText = "Số Nhân Viên";
-            this.SoNhanVien.Name = "SoNhanVien";
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(670, 234);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 11;
-            this.button3.Text = "Xóa";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(351, 234);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 10;
-            this.button2.Text = "Cập Nhật";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // bt_Them
-            // 
-            this.bt_Them.Location = new System.Drawing.Point(32, 234);
-            this.bt_Them.Name = "bt_Them";
-            this.bt_Them.Size = new System.Drawing.Size(75, 23);
-            this.bt_Them.TabIndex = 9;
-            this.bt_Them.Text = "Thêm";
-            this.bt_Them.UseVisualStyleBackColor = true;
-            this.bt_Them.Click += new System.EventHandler(this.bt_Them_Click);
+            this.btnThem.Location = new System.Drawing.Point(43, 288);
+            this.btnThem.Margin = new System.Windows.Forms.Padding(4);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(100, 28);
+            this.btnThem.TabIndex = 9;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
             // 
             // txtSoLNhanVien
             // 
-            this.txtSoLNhanVien.Location = new System.Drawing.Point(525, 105);
+            this.txtSoLNhanVien.Location = new System.Drawing.Point(700, 129);
+            this.txtSoLNhanVien.Margin = new System.Windows.Forms.Padding(4);
             this.txtSoLNhanVien.Name = "txtSoLNhanVien";
-            this.txtSoLNhanVien.Size = new System.Drawing.Size(218, 22);
+            this.txtSoLNhanVien.Size = new System.Drawing.Size(289, 26);
             this.txtSoLNhanVien.TabIndex = 8;
             // 
             // txtTenPhongBan
             // 
-            this.txtTenPhongBan.Location = new System.Drawing.Point(136, 171);
+            this.txtTenPhongBan.Location = new System.Drawing.Point(181, 210);
+            this.txtTenPhongBan.Margin = new System.Windows.Forms.Padding(4);
             this.txtTenPhongBan.Name = "txtTenPhongBan";
-            this.txtTenPhongBan.Size = new System.Drawing.Size(218, 22);
+            this.txtTenPhongBan.Size = new System.Drawing.Size(289, 26);
             this.txtTenPhongBan.TabIndex = 7;
             // 
             // txtMaPhongBan
             // 
-            this.txtMaPhongBan.Location = new System.Drawing.Point(136, 105);
+            this.txtMaPhongBan.Location = new System.Drawing.Point(181, 129);
+            this.txtMaPhongBan.Margin = new System.Windows.Forms.Padding(4);
             this.txtMaPhongBan.Name = "txtMaPhongBan";
-            this.txtMaPhongBan.Size = new System.Drawing.Size(218, 22);
+            this.txtMaPhongBan.Size = new System.Drawing.Size(289, 26);
             this.txtMaPhongBan.TabIndex = 6;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(389, 108);
+            this.label6.Location = new System.Drawing.Point(519, 133);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(130, 16);
+            this.label6.Size = new System.Drawing.Size(162, 20);
             this.label6.TabIndex = 5;
             this.label6.Text = "Số Lượng Nhân Viên";
             // 
@@ -172,9 +170,10 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(29, 174);
+            this.label5.Location = new System.Drawing.Point(39, 214);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(101, 16);
+            this.label5.Size = new System.Drawing.Size(124, 20);
             this.label5.TabIndex = 4;
             this.label5.Text = "Tên Phòng Ban";
             // 
@@ -182,39 +181,64 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(29, 108);
+            this.label4.Location = new System.Drawing.Point(39, 133);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(96, 16);
+            this.label4.Size = new System.Drawing.Size(119, 20);
             this.label4.TabIndex = 3;
             this.label4.Text = "Mã Phòng Ban";
             // 
-            // lbmachinhanh
+            // lbMaChiNhanh
             // 
-            this.lbmachinhanh.AutoSize = true;
-            this.lbmachinhanh.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbmachinhanh.Location = new System.Drawing.Point(126, 42);
-            this.lbmachinhanh.Name = "lbmachinhanh";
-            this.lbmachinhanh.Size = new System.Drawing.Size(45, 16);
-            this.lbmachinhanh.TabIndex = 2;
-            this.lbmachinhanh.Text = "label3";
+            this.lbMaChiNhanh.AutoSize = true;
+            this.lbMaChiNhanh.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbMaChiNhanh.Location = new System.Drawing.Point(168, 52);
+            this.lbMaChiNhanh.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbMaChiNhanh.Name = "lbMaChiNhanh";
+            this.lbMaChiNhanh.Size = new System.Drawing.Size(53, 20);
+            this.lbMaChiNhanh.TabIndex = 2;
+            this.lbMaChiNhanh.Text = "label3";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(29, 42);
+            this.label2.Location = new System.Drawing.Point(39, 52);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(91, 16);
+            this.label2.Size = new System.Drawing.Size(115, 20);
             this.label2.TabIndex = 1;
             this.label2.Text = "Mã Chi Nhánh";
             // 
+            // MaPhongBan
+            // 
+            this.MaPhongBan.DataPropertyName = "Ma_PhongBan";
+            this.MaPhongBan.HeaderText = "Mã Phòng Ban";
+            this.MaPhongBan.MinimumWidth = 6;
+            this.MaPhongBan.Name = "MaPhongBan";
+            // 
+            // TenPhongBan
+            // 
+            this.TenPhongBan.DataPropertyName = "Ten_PhongBan";
+            this.TenPhongBan.HeaderText = "Tên Phòng Ban";
+            this.TenPhongBan.MinimumWidth = 6;
+            this.TenPhongBan.Name = "TenPhongBan";
+            // 
+            // SoNhanVien
+            // 
+            this.SoNhanVien.DataPropertyName = "SoLuongNV";
+            this.SoNhanVien.HeaderText = "Số Nhân Viên";
+            this.SoNhanVien.MinimumWidth = 6;
+            this.SoNhanVien.Name = "SoNhanVien";
+            // 
             // CT_PhongBan
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "CT_PhongBan";
-            this.Size = new System.Drawing.Size(799, 478);
+            this.Size = new System.Drawing.Size(1065, 588);
             this.Load += new System.EventHandler(this.CT_PhongBan_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
@@ -227,11 +251,11 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Label lbmachinhanh;
+        private System.Windows.Forms.Label lbMaChiNhanh;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button bt_Them;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnCapNhat;
+        private System.Windows.Forms.Button btnThem;
         private System.Windows.Forms.TextBox txtSoLNhanVien;
         private System.Windows.Forms.TextBox txtTenPhongBan;
         private System.Windows.Forms.TextBox txtMaPhongBan;
--- DuAn_LMB_NhanSu/US_Control/CT_PhongBan.cs
+++ DuAn_LMB_NhanSu/US_Control/CT_PhongBan.cs
@@ -14,7 +14,56 @@ namespace DuAn_LMB_NhanSu.US_Control
     {
         public delegate DataGridViewRow drvChiNhanh();
         public drvChiNhanh getChinhanh;
-        DataTable dt_phongban = new DataTable();
+        DataTable dt = new DataTable();
+        string mapb = null, tenpb = null, sonv = null;
+
+        //Hàm load dữ liệu
+        public void load()
+        {
+            DataGridViewRow drv_chinhanh = getChinhanh();
+            lbMaChiNhanh.Text = drv_chinhanh.Cells[0].Value.ToString();
+            string query = "select Ma_PhongBan,Ten_PhongBan,SoLuongNV from PHONGBAN where Ma_ChiNhanh = '" + lbMaChiNhanh.Text + "'";
+            dt = Dataprovider.getDatatTable(query);
+            Luoi_PhongBan.DataSource = dt;
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtMaPhongBan.Text))
+            {
+                if (!string.IsNullOrEmpty(txtTenPhongBan.Text))
+                {
+                    string insert = "insert into PHONGBAN(Ma_PhongBan,Ten_PhongBan,SoLuongNV,Ma_ChiNhanh) values ('" + txtMaPhongBan.Text + "',N'" + txtTenPhongBan.Text + "','" + txtSoLNhanVien.Text + "','" + lbMaChiNhanh.Text + "')";
+                    dt = Dataprovider.getDatatTable(insert);
+                    load();
+                }
+            }
+        }
+
+        private void btnCapNhat_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtMaPhongBan.Text))
+            {
+                if (!string.IsNullOrEmpty(txtTenPhongBan.Text))
+                {
+                    string update = "update PHONGBAN set Ten_PhongBan = N'" + txtTenPhongBan.Text + "',SoLuongNV = '" + txtSoLNhanVien.Text + "',Ma_ChiNhanh = '" + lbMaChiNhanh.Text + "' where Ma_PhongBan = '" + txtMaPhongBan.Text + "'";
+                    dt = Dataprovider.getDatatTable(update);
+                    int ktra = Dataprovider.Update(dt);
+                    load();
+                }
+            }
+        }
+
+        private void Luoi_PhongBan_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            mapb = Luoi_PhongBan.CurrentRow.Cells[0].Value.ToString();
+            tenpb = Luoi_PhongBan.CurrentRow.Cells[1].Value.ToString();
+            sonv = Luoi_PhongBan.CurrentRow.Cells[2].Value.ToString();
+            txtMaPhongBan.Text = mapb;
+            txtSoLNhanVien.Text = sonv;
+            txtTenPhongBan.Text = tenpb;
+        }
+
         public CT_PhongBan()
         {
             InitializeComponent();
@@ -22,13 +71,8 @@ namespace DuAn_LMB_NhanSu.US_Control
 
         private void CT_PhongBan_Load(object sender, EventArgs e)
         {
-            DataGridViewRow drv_chinhanh = getChinhanh();
-            lbmachinhanh.Text = drv_chinhanh.Cells[0].Value.ToString();
+            load();
         }
 
-        private void bt_Them_Click(object sender, EventArgs e)
-        {
-
-        }
     }
 }
